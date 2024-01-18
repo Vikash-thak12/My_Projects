a5e0a9216d16cf385b87caa4a4b7ebfe7346ddae
Vikash-thak12 <thakvikash622@gmail.com>
2024-01-18 22:55:38 +0530

    add a new function in the javascript that will remove one by one digit

--- Calculator/index.js
+++ Calculator/index.js
@@ -10,6 +10,16 @@ function clearDisplay()
     display.value = "";
 }
 
+function removeLastDigit() {
+    var display = document.getElementById('display');
+    var currentValue = display.value;
+
+    // Check if there is anything to remove
+    if (currentValue.length > 0) {
+      // Remove the last character
+      display.value = currentValue.slice(0, -1);
+    }
+  }
 function calculate()
 {
    try
