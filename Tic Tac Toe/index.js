0baf3f969f0c1e16d2725405f2fd4afe625ea157
Vikash-thak12 <thakvikash622@gmail.com>
2024-01-20 01:30:43 +0530

    final commit

--- Tic Tac Toe/index.js
+++ Tic Tac Toe/index.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let reset_btn = document.querySelector("#reset-btn");
+let NewGame = document.querySelector("#newgame-btn");
+let msg = document.querySelector("#msg");
+let msg_container = document.querySelector(".msg-container");
 
 let turnO = true;
 
@@ -15,6 +18,44 @@ const winnerpatterns = [
 ]
 
 
+
+// This function is for disabling the boxes after the winner is declared
+const disabledboxes = () => {
+    for(let box of boxes)
+    {
+        box.disabled = true;
+    }
+}
+
+
+// This function is for enabling the box after new game 
+const enableboxes = () => {
+    for(let box of boxes)
+    {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+
+// This function is for reseting the game 
+const ResetGame = () => {
+    turnO = true;
+    enableboxes();
+    msg_container.classList.add("hide");
+}
+
+
+// This function is for showing the winner name 
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msg_container.classList.remove("hide");
+    disabledboxes();
+}
+
+
+
+// This function is for checking the winner 
 const checkwinner = () =>
 {
     for(let pattern of winnerpatterns) {
@@ -26,12 +67,16 @@ const checkwinner = () =>
         {
             if(pos1val === pos2val && pos2val === pos3val)
             {
-                console.log("winner");
+                console.log("winner", pos1val);
+                showWinner(pos1val);
             }
         }
     }
 }
 
+
+
+//This is the loop where each box will be given a value like x or O
 boxes.forEach((box) =>
 {
     box.addEventListener("click", () => {
@@ -51,3 +96,10 @@ boxes.forEach((box) =>
         checkwinner();
     })
 })
+
+
+
+
+// These are the two btn where resetgame function will be triggered
+reset_btn.addEventListener("click", ResetGame);
+NewGame.addEventListener("click", ResetGame);
