7ad4b6960754f229ab81b637a169042ff655280d
Vikash-thak12 <thakvikash622@gmail.com>
2024-01-21 07:50:52 +0530

    Final commit

--- Stone, Paper and Scissor/index.js
+++ Stone, Paper and Scissor/index.js
@@ -2,6 +2,9 @@ let userscore = 0;
 let compscore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const userscorevalue = document.querySelector("#user-score");
+const compscorevalue = document.querySelector("#comp-score");
 
 
 
@@ -15,16 +18,26 @@ const genCompChoice = () => {
 const drawGame = () =>
 {
     console.log("Game was Drawn:");
+    msg.innerText = "Game Draw. Play Again";
+    msg.style.background = "Black";
 }
 
-const showWinner = (uWin) => {
+const showWinner = (uWin, userchoice, compChoice) => {
     if(uWin)
     {
+        userscore++;
+        userscorevalue.innerText = (userscore);
         console.log(" You are the Winner: ");
+        msg.innerText = ` You win. Your ${userchoice} beats ${compChoice} `;
+        msg.style.background = "Green";
     }
     else 
     {
+        compscore++;
+        compscorevalue.innerText = (compscore);
         console.log(" You lose the Game: ");
+        msg.innerText = `You lose. ${compChoice} beats your ${userchoice}`;
+        msg.style.background = "Red";
     }
 }
 
@@ -55,7 +68,7 @@ const playGame = (userchoice) => {
         {
             uWin = compChoice === "rock" ? false : true;
         }
-        showWinner(uWin);
+        showWinner(uWin, userchoice, compChoice);
     }
     
 }
