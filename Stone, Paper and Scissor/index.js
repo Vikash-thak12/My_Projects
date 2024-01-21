c825d64f1f401552802b817d34f9e322a25fac34
Vikash-thak12 <thakvikash622@gmail.com>
2024-01-21 06:38:31 +0530

    add a new function that will generate random choic for the computer

--- Stone, Paper and Scissor/index.js
+++ Stone, Paper and Scissor/index.js
@@ -6,18 +6,22 @@ const choices = document.querySelectorAll(".choice");
 
 
 const genCompChoice = () => {
-    const options = ["paper", "rock", "Scissor"]
+    const options = ["paper", "rock", "Scissor"];
+    const randix = Math.floor(Math.random()*3);
+    return options[randix];
 }
 
 
 const playGame = (userchoice) => {
     console.log("User choice =", userchoice);
-    const compChoice = 
+    const compChoice = genCompChoice();
+    console.log("Computer choice =", compChoice);
 }
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userchoice = choice.getAttribute("id")
-        console.log(userchoice,"was clicked")
+        // console.log(userchoice,"was clicked");
+        playGame(userchoice);
     })
 })
