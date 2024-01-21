afa609b3c3170cbc52a6e7290c7d170c9ce072fa
Vikash-thak12 <thakvikash622@gmail.com>
2024-01-21 07:27:01 +0530

    core logic of the game have been setup using JS

--- Stone, Paper and Scissor/index.js
+++ Stone, Paper and Scissor/index.js
@@ -6,9 +6,26 @@ const choices = document.querySelectorAll(".choice");
 
 
 const genCompChoice = () => {
-    const options = ["paper", "rock", "Scissor"];
+    const options = ["paper", "rock", "scissor"];
     const randix = Math.floor(Math.random()*3);
     return options[randix];
+}
+
+
+const drawGame = () =>
+{
+    console.log("Game was Drawn:");
+}
+
+const showWinner = (uWin) => {
+    if(uWin)
+    {
+        console.log(" You are the Winner: ");
+    }
+    else 
+    {
+        console.log(" You lose the Game: ");
+    }
 }
 
 
@@ -16,7 +33,35 @@ const playGame = (userchoice) => {
     console.log("User choice =", userchoice);
     const compChoice = genCompChoice();
     console.log("Computer choice =", compChoice);
+
+    if(userchoice === compChoice)
+    {
+        drawGame();
+    }
+    else
+    {
+        let uWin = true;
+        if(userchoice === "paper")
+        {
+            // rock and scissor
+            uWin = compChoice === "scissor" ? false: true;
+        }
+        else if(userchoice === "rock")
+        {
+            // paper and scissor
+            uWin = compChoice === "paper" ? false : true;
+        }
+        else
+        {
+            uWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(uWin);
+    }
+    
 }
+
+
+
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
