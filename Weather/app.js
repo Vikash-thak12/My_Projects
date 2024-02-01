bc9c0e2de473defdb83b49479df7af323c2f51a3
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-01 10:27:59 +0530

    declare variabel in JS and small change in html

---
+++ Weather/app.js
@@ -0,0 +1,4 @@
+const weatherform  = document.querySelector(".weatherform");
+const entercity  = document.querySelector(".entercity");
+const card  = document.querySelector(".card");
+const API  = "04aa3e589306de0671ac9380e78112fd";
