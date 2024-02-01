552b697f0728c07fbebca80de78e928f1cf46856
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-01 15:35:59 +0530

    Added two funtion in the js for getting errormsg and submiting the name of the city

--- Weather/app.js
+++ Weather/app.js
@@ -2,3 +2,46 @@ const weatherform  = document.querySelector(".weatherform");
 const entercity  = document.querySelector(".entercity");
 const card  = document.querySelector(".card");
 const API  = "04aa3e589306de0671ac9380e78112fd";
+
+weatherform.addEventListener("submit", async event => {
+
+    event.preventDefault();
+    const city = entercity.value;
+
+    if (city) {
+        try {
+            const weatherdata = await getWeatherData(city);
+            displayinfo(data);
+        }
+        catch(error) {
+            console.error(error);
+            displayerror(error);
+        }
+
+    } else {
+        displayerror("Please Enter a city");
+    }
+});
+
+async function getWeatherData(city) {
+
+}
+
+function displayinfo(data) {
+
+}
+
+function getWeatherEmoji(weatherId) {
+
+}
+
+function displayerror(message){
+
+    const errormsg = document.createElement("p");
+    errormsg.textContent = message;
+    errormsg.classList.add("displayerror");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errormsg);
+}
