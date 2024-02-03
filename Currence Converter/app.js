3f1b000b227fd6a0429f259753f7b2d962ca85a7
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-03 11:44:02 +0530

    remove select options from html file and create those options in js file

--- Currence Converter/app.js
+++ Currence Converter/app.js
@@ -1,7 +1,22 @@
 const BASE_URL =
   "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
 
-for(let code in countryList)
-{
-    console.log(code);
+
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newoption = document.createElement("Option");
+        newoption.innerText = currCode;
+        newoption.value = currCode;
+        if(select.name === "from" && currCode === "USD")
+        {
+            newoption.selected = "selected";
+        } else if(select.name === "To" && currCode === "INR")
+        {
+            newoption.selected = "selected";
+        }
+        select.append(newoption);
+    }
 }
