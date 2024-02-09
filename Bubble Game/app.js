7e347dd208c267076669bcb3aeca8640a7f7dfb2
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-09 15:37:21 +0530

    Add Math.random() function

--- Bubble Game/app.js
+++ Bubble Game/app.js
@@ -1,7 +1,8 @@
 var clutter = "";
 for(var i = 1; i<=140; i++)
 {
-   clutter +=  `<div class="circle"><span>5</span></div>`;
+   var num = Math.floor(Math.random()*10);
+   clutter +=  `<div class="circle"><span>${num}</span></div>`;
 }
 
 document.querySelector("#second").innerHTML = clutter;
