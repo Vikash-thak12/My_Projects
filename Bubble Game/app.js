c44280d7d95e4eeda483dedb56298ebd5dafcfe7
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-09 22:44:08 +0530

    Added two functions

--- Bubble Game/app.js
+++ Bubble Game/app.js
@@ -1,14 +1,34 @@
+var timer = 60;
+var score = 0; 
+var rn;
 
 //This is the function for making bubble in the second page
 function makeBubble() {
     var clutter = "";
-for(var i = 1; i<=140; i++)
-{
-   var num = Math.floor(Math.random()*10);
-   clutter +=  `<div class="circle"><span>${num}</span></div>`;
+    for (var i = 1; i <= 140; i++) {
+        var num = Math.floor(Math.random() * 10);
+        clutter += `<div class="circle"><span>${num}</span></div>`;
+    }
+
+    document.querySelector("#second").innerHTML = clutter;
 }
 
-document.querySelector("#second").innerHTML = clutter;
+function runtimer() {
+    var timerint = setInterval( function () {
+        if(timer > 0) {
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        } else {
+            clearInterval(timerint);
+        }
+    },1000);
+}
+
+function hitscore () {
+   rn =  Math.floor(Math.random()*10);
+   document.querySelector("#hitscore").textContent = rn;
 }
 
 makeBubble();
+runtimer();
+hitscore();
