8f45d7d11bb5caba8d315cc94fd74398d5659658
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-11 20:32:38 +0530

    making the list of songs

--- Spotify Clone/app.js
+++ Spotify Clone/app.js
@@ -9,20 +9,29 @@ async function getSongs() {
     for (i = 0; i < as.length; i++) {
         const element = as[i];
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href)
+            songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs;
 }
 
 async function main() {
+    //list of all the songs
     let songs = await getSongs();
     console.log(songs);
+
+    let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+    for(const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20", " ")} </li>`;
+    }
 
     //play the first song
     var audio = new Audio(songs[0]);
     // audio.play();
 
+    audio.addEventListener("loadeddata" , () => {
+        console.log(audio.duration, audio.currentSrc, audio.currentTime);
+    })
 }
 
 main()
