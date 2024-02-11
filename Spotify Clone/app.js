5420bedc4f8fdcb50834326ee1b0e7a595afd341
Vikash-thak12 <thakvikash622@gmail.com>
2024-02-11 18:42:10 +0530

    finally added the funciton of all songs to be played

--- Spotify Clone/app.js
+++ Spotify Clone/app.js
@@ -1,6 +1,44 @@
-async function main() {
-    var a = await fetch("http://127.0.0.1:5500/songs/");
-    let response  = await a.text();
-    console.log(response);
+console.log("Let's write JavaScript");
+async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+    let div = document.createElement("div");
+    let val = div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+    for (i = 0; i < as.length; i++) {
+        const element = as[i];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href)
+        }
+    }
+    return songs;
 }
-main();
+
+async function main() {
+
+    //getting the list of all the songs
+    let songlist = await getSongs();
+    console.log(songlist);
+
+    // Play the songs in the array sequentially
+    let index = 0;
+
+    function playNextSong() {
+        if (index < songlist.length) {
+            var audio = new Audio(songlist[index]);
+            audio.addEventListener('ended', function() {
+                index++;
+                playNextSong();
+            });
+            audio.play();
+        }
+    }
+
+    // Wait for user interaction
+    document.addEventListener("click", function() {
+        playNextSong();
+    }, { once: true }); // The event listener will only trigger once
+}
+
+main()
