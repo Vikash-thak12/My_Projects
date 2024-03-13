79c3b10cf54a9789a3938291bd4a285891762190
Vikash Thakur <thakvikash622@gmail.com>
2024-03-13 18:11:33 +0530

    Done with the sidebar of the webpage

--- Gemini-Clone/Gemini-Clone/src/component/Sidebar/Sidebar.jsx
+++ Gemini-Clone/Gemini-Clone/src/component/Sidebar/Sidebar.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css'
-import {assets} from '../../assets/assets';
+import { assets } from '../../assets/assets';
 
 const Sidebar = () => {
+
+  const [extended, setExtended] = useState(false);
+
   return (
     <div className="Sidebar">
-        <div className="top">
-            <img className='menu' src={assets.menu_icon} />
-            <div className="chat">
-              <img src={assets.plus_icon}  />
-              <p>New Chat</p>
-            </div>
+      <div className="top">
 
-            <div className="recent">
-              <p>Recent</p>
-              <div className="recent-history">
-                <img src={assets.message_icon}  />
-                <p>Learn about React...</p>
-              </div>
-            </div>
 
+        <img onClick={() => setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} />
+        <div className="chat">
+          <img src={assets.plus_icon} />
+          {extended?<p>New Chat</p>:null}
         </div>
-        <div className="bottom">
 
+
+        {extended? 
+        <div className="recent">
+          <p>Recent</p>
+          <div className="recent-history">
+            <img src={assets.message_icon} />
+            <p>Learn about React...</p>
+          </div>
+        </div> 
+        :null}
+       
+
+      </div>
+      <div className="bottom">
+        <div className="bottom-item">
+          <img src={assets.question_icon} />
+          {extended?<p>Help</p>:null}
         </div>
+        <div className="bottom-item">
+          <img src={assets.history_icon} />
+          {extended?<p>Activity</p>:null}
+        </div>
+        <div className="bottom-item">
+          <img src={assets.setting_icon} />
+          {extended?<p>Setting</p>:null}
+        </div>
+      </div>
     </div>
   );
 };
