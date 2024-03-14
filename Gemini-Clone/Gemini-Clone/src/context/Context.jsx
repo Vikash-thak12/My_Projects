93b677f9630124fa7f6f21331bb774cadd001f93
Vikash Thakur <thakvikash622@gmail.com>
2024-03-14 23:41:58 +0530

    Declare variables

--- Gemini-Clone/Gemini-Clone/src/context/Context.jsx
+++ Gemini-Clone/Gemini-Clone/src/context/Context.jsx
@@ -1,18 +1,26 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
-        const onSent = async (prompt) => {
-            await runChat(prompt)
-        }
+    const [input, setInput] = useState("");
+    const [recentPrompts, setRecentPrompts] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showresult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
 
-        onSent("What is Software Engineering")
-        const contextvalue = {
+    const onSent = async (prompt) => {
+        await runChat(prompt)
+    }
 
-        }
+    // onSent("What is Software Engineering")
+
+    const contextvalue = {
+
+    }
 
     return (
         <Context.Provider value={contextvalue}>
