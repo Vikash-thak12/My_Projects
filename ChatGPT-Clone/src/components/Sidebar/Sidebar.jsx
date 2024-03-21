81f224ed10e10b43a8b2e5cd7c4d5d2ada2e7c70
Vikash Thakur <thakvikash622@gmail.com>
2024-03-21 18:14:44 +0530

    Doen with the sidebar

--- ChatGPT-Clone/src/components/Sidebar/Sidebar.jsx
+++ ChatGPT-Clone/src/components/Sidebar/Sidebar.jsx
@@ -6,6 +6,8 @@ const Sidebar = () => {
   return (
     <>
       <div className="main">
+
+        {/* upper part of the sidebar */}
         <div className="upper">
             <div className="left">
                 <img src={assets.chatgpt} alt="" />
@@ -14,6 +16,23 @@ const Sidebar = () => {
             <img src={assets.chat} alt="" />
         </div>
 
+        {/* middle part of the sidebar */}
+        <div className="middle">
+            <div className="mid-1">
+                <img src={assets.add}  />
+                <p>New Chat</p>
+            </div>
+            <div className="mid-2">
+                <img src={assets.message}  />
+                <p>What is programming?</p>
+            </div>
+            <div className="mid-2">
+                <img src={assets.message}  />
+                <p>How to use API?</p>
+            </div>
+        </div>
+
+        {/* Bottom part of the sidebar */}
         <div className="bottom">
             <div className="first">
                 <img src={assets.home} />
