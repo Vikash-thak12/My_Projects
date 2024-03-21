f0190f1fa9d5eb37546086c3ad28c1114dd2343e
Vikash Thakur <thakvikash622@gmail.com>
2024-03-21 16:32:03 +0530

    Added svgs

--- ChatGPT-Clone/src/App.jsx
+++ ChatGPT-Clone/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     
+     <div className="main">
+      Hi there what you are doing??
+     </div>
     </>
   )
 }
