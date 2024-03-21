cb8a4f22ded71afa3bd182400aeff5b5f0e3b8fd
Vikash Thakur <thakvikash622@gmail.com>
2024-03-21 22:59:16 +0530

    Done with the main part of the body

--- ChatGPT-Clone/src/components/Main/Main.jsx
+++ ChatGPT-Clone/src/components/Main/Main.jsx
@@ -4,18 +4,34 @@ import { assets } from '../../assets/assets';
 
 
 const Main = () => {
-  return (
-    <>
-      <div className="main">
-        <div className="content">
-            <div className="sender">
-                <input type="text" placeholder='Send a message' />
-                <img src={assets.send} />
+    return (
+        <>
+            <div className="main">
+                <div className="content">
+                    {/* chat responding part */}
+                    <div className="chat">
+                        <div className="firstchat">
+                            <img src={assets.user_icon} />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam repudiandae.
+                            </p>
+                        </div>
+                        <div className="secondchat bot">
+                            <img src={assets.chatgptLogo} />
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam repudiandae, cumque rerum maxime optio quisquam debitis, quis ea impedit pariatur consequuntur tempora. Quisquam mollitia deserunt, illum voluptate a voluptatum magnam.
+                                
+                            </p>
+                        </div>
+                    </div>
+
+                    {/* prompts sending part */}
+                    <div className="sender">
+                        <input type="text" placeholder='Send a message' />
+                        <img src={assets.send} />
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Main;
